0e872a0b10db22947bf9f74c99024984dd8bd4a6
AbdulQadirImran <160231143+AbdulQadirImran@users.noreply.github.com>
2024-02-21 19:08:35 +0500

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const input = document.querySelector("input"),
   guess = document.querySelector(".guess"),
   checkButton = document.querySelector("button"),
   remainChances = document.querySelector(".chances");
-
 // Set the focus on input field
 input.focus();
 
